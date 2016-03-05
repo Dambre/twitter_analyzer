4ce44926747fd75d3d273b4f2e3cbdd7f694938d
Lukas <lukas.dambr@gmail.com>
2016-03-05 12:23:27 +0200

    Delete local_settings.py

--- hashbattle/local_settings.py
+++
@@ -1,9 +0,0 @@
-'''
-This file contains sensitive data
-'''
-
-
-consumer_key = "b9lMwkyWSLmzTf7nNKTaKpJR5"
-consumer_secret = "eTpeUCwtz2enlzUP6WqfMyeasX9ZT32kKHl6KZvJEJoHqeloop"
-access_token = "2895801814-vYUlcBodcanAuHxBD6VlkKepnDq92B3qYNHqAyS"
-access_token_secret = "Y6jc4o8cdQoo1mebRhTlFc705GjMtBl9TX9hTCTjhdokm"
