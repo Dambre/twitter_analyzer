ec7a229253a018cf25635a252ddddaee25255234
lukas <lukas.dambr@gmail.com>
2016-09-09 14:18:38 +0300

    word_usage admin

--- battle_app/admin.py
+++ battle_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+class WordUsageAdmin(admin.ModelAdmin):
+	model = WordUsage
+	list_display = ('word', 'retweets', 'likes', 'timestamp')
 
 admin.site.register(Word)
-admin.site.register(WordUsage)
+admin.site.register(WordUsage, WordUsageAdmin)
 admin.site.register(Synonym)
